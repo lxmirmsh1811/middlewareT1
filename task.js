be1389af783d45aa225d63dda2658ce7f889dbf8
Lakshmi Ramesh <lakshmiramesh@Lakshmis-MacBook-Air.local>
2019-10-29 19:46:59 +0100

    initial commit

---
+++ task.js
@@ -0,0 +1,15 @@
+
+function convert(){
+  var val = document.getElementById("eu").value;
+  var selected_value = document.getElementById("input").value;
+  var ans;
+  if (selected_value == "usd") ans = (val * 1.11).toFixed(2);
+  if (selected_value == "cad") ans = (val * 1.45).toFixed(2);
+  if (selected_value == "inr") ans = val * 78.53.toFixed(2);
+  document.getElementById("converted").value = ans
+
+}
+
+function reset(){
+  document.getElementById('clear').reset();
+}
